e6edf4ba9c0a7c06726928ddf35bba4aa3d492ef
Matheussmaced <mateusbyakuya3@gmail.com>
2023-10-18 12:05:02 -0300

    feat: publications container

--- src/pages/Home/components/Publications/index.tsx
+++ src/pages/Home/components/Publications/index.tsx
@@ -1,6 +1,23 @@
-import { PublicationSubtitle, PublicationsContainer, SearchBar } from './styles'
+import {
+  PublicationSubtitle,
+  PublicationsContainer,
+  SearchBar,
+  UsePublicationsHeader,
+  UserPublicationsContainer,
+  UserPublicationsDescription,
+} from './styles'
 
 export const Publications = () => {
+  const informsContainer = [
+    {
+      id: 1,
+      title: 'JavaScript data types and data structures',
+      publishedAt: 'Há 1 dia',
+      description:
+        'Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in ',
+    },
+  ]
+
   return (
     <PublicationsContainer>
       <PublicationSubtitle>
@@ -11,6 +28,19 @@ export const Publications = () => {
       <form>
         <SearchBar type="text" placeholder="Buscar conteúdo" />
       </form>
+
+      {informsContainer.map((informs) => (
+        <UserPublicationsContainer key={informs.id}>
+          <UsePublicationsHeader>
+            <span>{informs.title}</span>
+            <p>{informs.publishedAt}</p>
+          </UsePublicationsHeader>
+
+          <UserPublicationsDescription>
+            <span>{informs.description}</span>
+          </UserPublicationsDescription>
+        </UserPublicationsContainer>
+      ))}
     </PublicationsContainer>
   )
 }
--- src/pages/Home/components/Publications/styles.ts
+++ src/pages/Home/components/Publications/styles.ts
@@ -30,8 +30,41 @@ export const SearchBar = styled.input`
 
   background-color: ${(props) => props.theme['base-input']};
   border: ${(props) => props.theme['base-border']};
+  color: ${(props) => props.theme.white};
 
   &::placeholder {
     color: ${(props) => props.theme['base-label']};
+  }
+`
+
+export const UserPublicationsContainer = styled.div`
+  width: 45%;
+  background-color: ${(props) => props.theme['base-post']};
+  margin: 3rem auto;
+  padding: 2rem;
+
+  border-radius: 18px;
+`
+
+export const UsePublicationsHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+
+  span {
+    font-size: 1.25rem;
+    width: 73%;
+    margin-bottom: 1.25rem;
+  }
+  p {
+    font-size: 0.875rem;
+    color: ${(props) => props.theme['base-span']};
+    font-size: 0.875rem;
+  }
+`
+
+export const UserPublicationsDescription = styled.div`
+  span {
+    color: ${(props) => props.theme['base-text']};
   }
 `
