bc790e6990241c8501f08ed086de35c6e208c4d1
Matheussmaced <mateusbyakuya3@gmail.com>
2023-10-18 19:25:59 -0300

    call of API

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,24 @@ import { Profile } from './components/Profile'
 import { Publications } from './components/Publications'
 import { HomeContainer } from './styles'
 
+import { useState, useEffect } from 'react'
+
 export const Home = () => {
+  const [userProfile, setUserProfile] = useState([])
+
+  async function loadProfileGithub() {
+    const response = await fetch('https://api.github.com/users/Matheussmaced')
+    const data = await response.json()
+
+    setUserProfile(data)
+  }
+
+  useEffect(() => {
+    loadProfileGithub()
+  }, [])
+
+  console.log(userProfile)
+
   return (
     <HomeContainer>
       <Profile />
